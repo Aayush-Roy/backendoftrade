019a609d42c01caf2e455e9e1eed93cbdb2661b0
aayush <ar0671362@gmail.com>
2026-03-21 22:09:12 +0530

    added bodyparser and cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import holdingRoute from "./routes/holding.route.js"
 import positionRoute from "./routes/position.route.js"
-import { prisma } from "./lib/prisma.js";
 dotenv.config();
 const app = express();
+app.use(bodyParser.json())
+app.use(cors());
+
 const PORT = process.env.PORT
 app.use("/api/holdings", holdingRoute);
 app.use("/api/positions", positionRoute);
